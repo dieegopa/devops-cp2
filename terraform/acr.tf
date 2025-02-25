e58f8dfae01254626f30bbb715d1147fd8293908
Diego Padilla <diegopadilla6@gmail.com>
2025-02-25 14:10:35 +0100

    Change acr to basic

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -2,6 +2,6 @@ resource "azurerm_container_registry" "acr" {
   name                = "diegodevopscp2"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
+  sku                 = "Basic"
   admin_enabled       = true
 }

65775a59d492d070e1e6991fc8bb90dc60285984
Diego Padilla <diegopadilla6@gmail.com>
2025-02-25 10:32:41 +0100

    Add vm creation

---
+++ terraform/vm.tf
@@ -0,0 +1,26 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "casopractico2-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    name                 = "casopractico2-os-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+}
